5c7f79ec784f00f6021e6865ed2c4b5dc48dbdfb
tabaesso <tatabaesso@gmail.com>
2020-10-17 04:41:05 -0300

    login de clientes funcionando e rotas adicionas ao routes.md

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import vehiclesRouter from './vehicles.routes';
 import customersRouter from './customers.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/veiculos', vehiclesRouter);
 routes.use('/clientes', customersRouter);
+routes.use('/login', sessionsRouter);
 
 export default routes;

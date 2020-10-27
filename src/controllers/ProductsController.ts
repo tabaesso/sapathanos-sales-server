55fa29988cc86e5d269303492a5b8169c0f91056
Schuansk Lucas Tenorio Torres <schuansk@hotmail.com.br>
2020-10-26 22:51:51 -0300

    relacao sellers products

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -6,72 +6,73 @@ import CreateProductService from '../services/CreateProductService';
 import UpdateProductStatusService from '../services/UpdateProductStatusService';
 
 export default class ProductsController {
-    async create(request: Request, response: Response) {
-        const {
-            category_id,
-            name,
-            description,
-            color,
-            material,
-            price
-        } = request.body;
+  async create(request: Request, response: Response) {
+    const {
+      seller_id,
+      category_id,
+      name,
+      description,
+      color,
+      material,
+      price
+    } = request.body;
 
+    const createProduct = new CreateProductService();
 
-        const createProduct = new CreateProductService();
+    const product = await createProduct.execute({
+      seller_id,
+      category_id,
+      name,
+      description,
+      color,
+      material,
+      price
+    });
 
-        const product = await createProduct.execute({
-            category_id,
-            name,
-            description,
-            color,
-            material,
-            price
-        });
+    return response.json(product);
+  }
 
-        return response.json(product);
-    }
+  async updateStatus(request: Request, response: Response) {
+      const { product_id } = request.params;
 
-    async updateStatus(request: Request, response: Response) {
-        const { product_id } = request.params;
+      const updateProductStatus = new UpdateProductStatusService();
 
-        const updateProductStatus = new UpdateProductStatusService();
+      const product = await updateProductStatus.execute({
+          product_id
+      });
 
-        const product = await updateProductStatus.execute({
-            product_id
-        });
+      return response.json(product);
+  }
 
-        return response.json(product);
-    }
+  async findActive(request: Request, response: Response) {
+      const productsRepository = getRepository(Product);
 
-    async findActive(request: Request, response: Response) {
-        const productsRepository = getRepository(Product);
+      const products = await productsRepository.find({
+          where: {status: 1}
+      })
 
-        const products = await productsRepository.find({
-            where: {status: 1}
-        })
+      return response.json(products);
+  }
 
-        return response.json(products);
-    }
+  async index(request: Request, response: Response) {
+      const productsRepository = getRepository(Product);
 
-    async index(request: Request, response: Response) {
-        const productsRepository = getRepository(Product);
+      const products = await productsRepository.find();
 
-        const products = await productsRepository.find();
+      return response.json(products);
+  }
 
-        return response.json(products);
-    }
+  async show(request: Request, response: Response) {
+      const { id } = request.params;
 
-    async show(request: Request, response: Response) {
-        const { id } = request.params;
+      /** CONSULTA USANDO QUERY BUILDER */
+      const product = await getConnection()
+          .getRepository(Product)
+          .createQueryBuilder('product')
+          .leftJoinAndSelect('product.size', 'size')
+          .where('product.id =:id', {id: id})
+          .getMany();
 
-        /** CONSULTA USANDO QUERY BUILDER */
-        const product = await getConnection()
-            .getRepository(Product)
-            .createQueryBuilder('product')
-            .leftJoinAndSelect('product.size', 'size')
-            .where('product.id =:id', {id: id})
-            .getMany();
-
-        return response.json(product);
-    }
+      return response.json(product);
+  }
 }

51a8bd66944a4a3d6e98abe87e0e8520466a0a5e
tabaesso <tatabaesso@gmail.com>
2020-11-10 10:35:14 -0300

    configs to server

--- ormconfig.js
+++ ormconfig.js
@@ -2,10 +2,10 @@ module.exports = {
   "type": 'postgres',
   "url": process.env.DATABASE_URL,
   "entities": [
-    "dist/src/models/**/*.js",
+    "./src/models/**/*.js",
   ],
   "migrations": [
-    "dist/src/database/migrations/**/*.js",
+    "./src/database/migrations/**/*.js",
   ],
   "cli": {
     "migrationsDir": 'src/database/migrations',

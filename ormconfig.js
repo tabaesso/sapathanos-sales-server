6c8b3f5d1dac07c46421b50c6934be988565562c
tabaesso <tatabaesso@gmail.com>
2020-11-10 17:26:02 -0300

    configs deploy

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,10 @@
 module.exports = {
   "type": 'postgres',
-  "url": process.env.DATABASE_URL,
+  "host": process.env.PG_HOST,
+  "port": process.env.PG_PORT,
+  "username": process.env.PG_USER,
+  "password": process.env.PG_PASSWORD,
+  "database": process.env.PG_DATABASE,
   "entities": [
     "./dist/models/models/*js",
   ],

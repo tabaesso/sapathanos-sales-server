022e7852e0fc1aa74ef7ecbb2c28b7f10667463f
tabaesso <tatabaesso@gmail.com>
2020-11-10 02:58:28 -0300

    alterações para subir para o servidor

--- src/server.ts
+++ src/server.ts
@@ -29,6 +29,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('Back tá on! ✨');
 });
